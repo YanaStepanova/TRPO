31db1d0f35517f90ae682d1b0cb2ac5810555be5
YanaStepanova <65772632+YanaStepanova@users.noreply.github.com>
2023-05-05 18:02:52 +0500

    Добавьте файлы проекта.

---
+++ TRPO/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace TRPO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.тМЦToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.испытанияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.документыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.каталогToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавлениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.параметрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.текущиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.переченьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.тМЦToolStripMenuItem,
+            this.испытанияToolStripMenuItem,
+            this.документыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // тМЦToolStripMenuItem
+            // 
+            this.тМЦToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.каталогToolStripMenuItem,
+            this.добавлениеToolStripMenuItem});
+            this.тМЦToolStripMenuItem.Name = "тМЦToolStripMenuItem";
+            this.тМЦToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.тМЦToolStripMenuItem.Text = "ТМЦ";
+            // 
+            // испытанияToolStripMenuItem
+            // 
+            this.испытанияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.параметрыToolStripMenuItem,
+            this.текущиеToolStripMenuItem});
+            this.испытанияToolStripMenuItem.Name = "испытанияToolStripMenuItem";
+            this.испытанияToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.испытанияToolStripMenuItem.Text = "Испытания";
+            // 
+            // документыToolStripMenuItem
+            // 
+            this.документыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.переченьToolStripMenuItem});
+            this.документыToolStripMenuItem.Name = "документыToolStripMenuItem";
+            this.документыToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.документыToolStripMenuItem.Text = "Документы";
+            // 
+            // каталогToolStripMenuItem
+            // 
+            this.каталогToolStripMenuItem.Name = "каталогToolStripMenuItem";
+            this.каталогToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.каталогToolStripMenuItem.Text = "Каталог";
+            // 
+            // добавлениеToolStripMenuItem
+            // 
+            this.добавлениеToolStripMenuItem.Name = "добавлениеToolStripMenuItem";
+            this.добавлениеToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.добавлениеToolStripMenuItem.Text = "Добавление";
+            // 
+            // параметрыToolStripMenuItem
+            // 
+            this.параметрыToolStripMenuItem.Name = "параметрыToolStripMenuItem";
+            this.параметрыToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.параметрыToolStripMenuItem.Text = "Параметры ";
+            // 
+            // текущиеToolStripMenuItem
+            // 
+            this.текущиеToolStripMenuItem.Name = "текущиеToolStripMenuItem";
+            this.текущиеToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.текущиеToolStripMenuItem.Text = "Текущие";
+            // 
+            // переченьToolStripMenuItem
+            // 
+            this.переченьToolStripMenuItem.Name = "переченьToolStripMenuItem";
+            this.переченьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.переченьToolStripMenuItem.Text = "Перечень";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public TextBox textBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem тМЦToolStripMenuItem;
+        private ToolStripMenuItem каталогToolStripMenuItem;
+        private ToolStripMenuItem добавлениеToolStripMenuItem;
+        private ToolStripMenuItem испытанияToolStripMenuItem;
+        private ToolStripMenuItem параметрыToolStripMenuItem;
+        private ToolStripMenuItem текущиеToolStripMenuItem;
+        private ToolStripMenuItem документыToolStripMenuItem;
+        private ToolStripMenuItem переченьToolStripMenuItem;
+    }
+}
---
+++ TRPO/Form1.cs
@@ -0,0 +1,16 @@
+namespace TRPO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "“≈ —“";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
